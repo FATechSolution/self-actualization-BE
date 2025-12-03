e24cd41278a26187fb75aa42cb11b4b06b9ffa2b
BakarDevMaster <bakardevmaster@gmail.com>
2025-12-03 12:48:42 +0500

    cors fixed

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ connectDB().catch((err) => {
 app.use((req, res, next) => {
   const allowedOrigins = process.env.ALLOWED_ORIGINS
     ? process.env.ALLOWED_ORIGINS.split(",")
-    : ["http://localhost:3000", "http://localhost:5173","http://localhost:3001"];
+    : ["http://localhost:3000", "http://localhost:5173","http://localhost:3001","https://self-admin-panel.vercel.app"];
 
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin) || !origin) {
